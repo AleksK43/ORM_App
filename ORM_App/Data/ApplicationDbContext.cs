66aeba04cccabfef98bc20242865ab6bb6bde4e7
Student <Student@DESKTOP-KGK4MVM>
2024-04-20 19:25:13 +0200

    Add project files.

---
+++ ORM_App/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ORM_App.Models;
+
+namespace ORM_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ORM_App.Models.ExerciseType> ExerciseType { get; set; } = default!;
+        public DbSet<ORM_App.Models.Exercise> Exercise { get; set; } = default!;
+        public DbSet<ORM_App.Models.Session> Session { get; set; } = default!;
+    }
+}
---
+++ ORM_App/Models/Exercise.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ORM_App.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        [Display(Name ="Obciążenie"),Range(1,250)]
+        public int Weight { get; set; }
+        [Display(Name = "Liczba Powtórzeń"), Range(1,100)]
+        public int NumOfReps { get; set; }
+        [Display(Name = "Liczba Serii"), Range(1,100)]
+        public int NumOfSeries { get; set; }
+        
+        public int  ExerciseTypeId { get; set; }
+        [Display(Name = "Rodaj Ćwiczeń")]
+        public virtual ExerciseType? ExerciseType { get; set; }
+        [Display(Name = "Sesja")]
+        public int SessionId { get; set; }
+        
+        public virtual Session? Session { get; set; }
+
+    }
+}
